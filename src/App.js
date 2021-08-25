155f89d0fb85c741f8476a3b9349cd2d938899ca
MARTINCHUNGA <Bsc-77-16@cc.ac.mw>
2021-08-24 23:21:35 -0700

    working searc component

--- src/App.js
+++ src/App.js
@@ -5,78 +5,104 @@ import './App.css'
 import SearchBar from './components/SearchBar'
 import Button from './components/Button'
 import Header from './components/Header'
-import { getAll } from './BooksAPI'
+import * as BooksAPI from './BooksAPI'
+import {Switch, Route, withRouter} from 'react-router-dom'
+
 
 class BooksApp extends React.Component {
+  constructor(props) {
+    super(props);
+    
+  }
   state = {
+
     /**
      * TODO: Instead of using this state variable to keep track of which page
      * we're on, use the URL in the browser's address bar. This will ensure that
      * users can use the browser's back and forward buttons to navigate between
      * pages, as well as provide a good URL they can bookmark and share.
      */
-    query:"",
+    
     books: [],
-    showSearchPage: false
-  }
-
-  upDateSearchBarState = (state) => {
-    this.setState({
-      showSearchPage : !state
-    })
+    showSearchPage: false,
+   
+    
   }
 
   //Get all the books here using getAll function defined in BooksAPI
   componentDidMount(){
-   getAll().then((response) => (
+   BooksAPI.getAll().then((response) => {
      this.setState({
        books: response
      })
-   ))
+    })
   }
 
   
   moveBookToShelf = (book,shelf) => {
+    const desiredBooks = this.state.books.map(myBook =>{
+      if(myBook.id === book.id) {
+        myBook.shelf = shelf;
+
+      }
+      return myBook;
+    })
     this.setState({
-      books: this.state.books.map(myBook => {
-        return myBook.id === book.id?(myBook.shelf = shelf):myBook
-      })
+      books : desiredBooks
     })
   }
 
-  updateQuery = (query) =>{
-    this.setState(() => ({
-      query : query.trim()
-    }))
-
+  
+  upDateSearchBarState = () => {
+    this.props.history.push("/search")
   }
 
-  render() {
-   
-    //console.log(this.state.books)
-
+  
+  render() {   
     return (
       <div className="app">
-        {this.state.showSearchPage ? (
-          <SearchBar 
-          searchPage={this.upDateSearchBarState}
-          updateQuery={this.updateQuery}
-          />
-        ) : (
+
+        <Switch>
+          <Route path="/search" component={()=>
+            <SearchBar 
+            
+            moveBookToShelf={this.moveBookToShelf}
+            books={this.state.books}
+            />
+          }/>
+
+          <Route path="/" component={()=>
+          <AllShelves  
+          books={this.state.books}
+          moveBookToShelf={this.moveBookToShelf}
+          />  }/>
+          
+       
+          {/* // <SearchBar 
+          // searchPage={this.upDateSearchBarState}
+          // moveBookToShelf={this.moveBookToShelf}
+          // books={this.state.books}
+          // />
+        
           <div className="list-books">
             <Header />
 
             {/* pass the books to the shelves as props from state*/}
-            <AllShelves  
+            {/* <AllShelves  
             books={this.state.books}
             moveBookToShelf={this.moveBookToShelf}
             />            
            <Button  searchPage={this.upDateSearchBarState}/>
           </div>
-        )}
-      </div>
+        
+        <
+        */} 
+
+        </Switch>
+        <Button  searchPage={this.upDateSearchBarState}/>
+      </div> 
     )
   }
 }
 
-export default BooksApp
+export default withRouter(BooksApp)
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,13 +1,85 @@
 
 import React,{Component} from 'react'
+import * as BooksAPI from '../BooksAPI'
+
+import {withRouter} from 'react-router-dom'
 
 class SearchBar extends Component{
-    render(){
-        return(
+
+  state= {
+    shelfBooks:[],
+    query:"",
+    searchedBooks:[],
+    sholeBooks:[]
+  }
+
+  componentDidMount(){
+    BooksAPI.getAll().then((response) => {
+      this.setState({
+        shelfBooks: response
+      })
+     })
+   }
+
+
+  updateQuery = (event) =>{
+    this.setState({
+      query : event.target.value
+    })  
+  }
+
+
+componentDidUpdate(){
+  if(this.state.query){
+    BooksAPI.search(this.state.query).then(res=>{
+      this.setState({
+          searchedBooks:res
+      })
+    })
+  }
+
+  if(!this.state.searchedBooks.error){
+    this.state.searchedBooks.forEach(book=>{
+      this.state.shelfBooks.filter(ourBook=>{
+        if(ourBook.id===book.id){
+          book.shelf = ourBook.shelf
+          this.setState({
+            searchedBooks: [...this.state.searchedBooks, book]
+          })
           
+        }
+      })
+    })
+ 
+  }
+}
+
+handleBookShelfChange = (book,shelf) => {
+  const desiredBooks = this.state.searchedBooks.map(myBook =>{
+    if(myBook.id === book.id) {
+      myBook.shelf = shelf;
+
+    }
+    return myBook;
+  })
+  this.setState({
+    searchedBooks : desiredBooks
+  })
+}
+returnHome = ()=>{
+this.props.history.push("/")
+}
+
+    render(){
+   
+      const booksToDisplay = this.state.searchedBooks
+      let currentShelf = "none"
+    
+        return(
+
             <div className="search-books">
             <div className="search-books-bar">
-              <button className="close-search" onClick={() => this.props.searchPage(true)}>Close</button>
+              <button className="close-search" onClick={this.returnHome}>Close</button>
               <div className="search-books-input-wrapper">
                 {/*
                   NOTES: The search from BooksAPI is limited to a particular set of search terms.
@@ -20,13 +92,38 @@ class SearchBar extends Component{
                 <input 
                 type="text" 
                 placeholder="Search by title or author"
-                value={this.props.query}
-                onChange={(event) => this.props.updateQuery(event.target.value)}/>
+                value={this.state.query}
+                onChange={(event) => this.updateQuery(event)}/>
 
               </div>
             </div>
             <div className="search-books-results">
-              <ol className="books-grid"></ol>
+              <ol className="books-grid">
+              {booksToDisplay.map(book => {
+                    return <li key={book.id}> 
+                     <div className="book">
+                    <div className="book-top">
+                       <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.smallThumbnail})` }}></div>
+                          <div className="book-shelf-changer">
+                             <select 
+                             defaultValue={currentShelf}
+                             //value={book.shelf}
+                             onChange={e => this.handleBookShelfChange(book, e.target.value)}
+                             >
+                                <option value="move" disabled>Move to...</option>
+                                <option value="currentlyReading">Currently Reading</option>
+                                <option value="wantToRead">Want to Read</option>
+                                <option value="read">Read</option>
+                                <option value="none">None</option>
+                              </select>
+                          </div>
+                        </div>
+                      <div className="book-title">{book.title}</div>
+                    <div className="book-authors">{book.authors}</div>
+                  </div>
+                  </li>
+                })}  
+              </ol>
             </div>
           </div>
 
@@ -34,4 +131,4 @@ class SearchBar extends Component{
     }
 }
 
-export default SearchBar
+export default withRouter(SearchBar)
